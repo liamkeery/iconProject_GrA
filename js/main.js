ada269911b010a5f9f7fca8ff04a7d4e61996281
liamkeery <liamkeery@gmail.com>
2018-02-21 20:39:53 -0500

    Updated

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 (() => {
   console.log('Fired!');
+
+  var iconSet = document.querySelectorAll('svg');
+
+  iconSet.forEach(icon => icon.addEventListener('click',logThisIcon));
+
+  function logThisIcon() {
+    console.log('clicked on this icon: ', this.id);
+  }
 })();
